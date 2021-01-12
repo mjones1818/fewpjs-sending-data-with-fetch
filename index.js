4a3d10d74b4ba1cc88caae314e884184b2a2f374
mjones1818 <michael.jones0532@gmail.com>
2021-01-12 09:52:53 -0600

    commit tree 8afc5839a477b456153ea0ac290c927d60aa8457

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Add your code here
+let fetchUrl = 'http://localhost:3000/users'
+
+function submitData (name,email) {
+  let formData = {
+    name: name,
+    email: email
+  }
+  let configObj = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify(formData)
+  }
+  fetchUsers(configObj);
+}
+
+function fetchUsers (configObj) {
+  fetch(fetchUrl, configObj)
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(object) {
+    console.log(object);
+    return object
+  })
+  .then(function(object) {
+    addIdToDom(object);
+  })
+  .catch(function (error){
+    document.body.innerHTML = error.message
+  })
+}
+
+function addIdToDom (object) {
+  document.body.innerHTML = object["id"]
+}
